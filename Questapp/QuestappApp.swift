313f75580e7c1025f89e6d6e7ec7144ff7f3b6b5
Jesse <jasjitgill26@gmail.com>
2023-06-22 15:50:40 -0700

    Initial Commit

---
+++ Questapp/QuestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestappApp.swift
+//  Questapp
+//
+//  Created by Jasjit Gill on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
